fbd86ba973f981a7bb83ec5e0e3cfa894e352500
Rafael <rafaelgremista2010@gmail.com>
2020-11-15 21:33:36 -0300

    MainScreen Implementation

--- src/model/bean/Eleitor.java
+++ src/model/bean/Eleitor.java
@@ -1,112 +1,117 @@
 package model.bean;
 
 import java.util.Date;
+import java.util.Objects;
 
-public class Eleitor {
+public class Eleitor implements Comparable<Eleitor> {
 
-	private Integer cod;
-	private String nome;
-	private Date dataNasc;
-	private String telefone;
-	private String endereco;
-	private String rg;
-	private String cpf;
-	private Integer matricula;
-	private Instituicao instituicao;
+    private Integer cod;
+    private String nome;
+    private Date dataNasc;
+    private String telefone;
+    private String endereco;
+    private String rg;
+    private String cpf;
+    private Integer matricula;
+    private Instituicao instituicao;
 
-	public Eleitor() {
-	}
+    public Eleitor() {
+    }
 
-	public Eleitor(String nome, Date dataNasc, String telefone, String endereco, String rg, String cpf,
-			Integer matricula, Instituicao instituicao) {
-		this.nome = nome;
-		this.dataNasc = dataNasc;
-		this.telefone = telefone;
-		this.endereco = endereco;
-		this.rg = rg;
-		this.cpf = cpf;
-		this.matricula = matricula;
-		this.instituicao = instituicao;
-	}
+    public Eleitor(String nome, Date dataNasc, String telefone, String endereco, String rg, String cpf,
+            Integer matricula, Instituicao instituicao) {
+        this.nome = nome;
+        this.dataNasc = dataNasc;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.rg = rg;
+        this.cpf = cpf;
+        this.matricula = matricula;
+        this.instituicao = instituicao;
+    }
 
-	public Integer getCod() {
-		return cod;
-	}
-	
-	public void setCod(Integer cod) {
-		this.cod = cod;
-	}
-	
-	public String getNome() {
-		return nome;
-	}
+    public Integer getCod() {
+        return cod;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setCod(Integer cod) {
+        this.cod = cod;
+    }
 
-	public Date getDataNasc() {
-		return dataNasc;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setDataNasc(Date dataNasc) {
-		this.dataNasc = dataNasc;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public String getTelefone() {
-		return telefone;
-	}
+    public Date getDataNasc() {
+        return dataNasc;
+    }
 
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
+    public void setDataNasc(Date dataNasc) {
+        this.dataNasc = dataNasc;
+    }
 
-	public String getEndereco() {
-		return endereco;
-	}
+    public String getTelefone() {
+        return telefone;
+    }
 
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 
-	public String getRg() {
-		return rg;
-	}
+    public String getEndereco() {
+        return endereco;
+    }
 
-	public void setRg(String rg) {
-		this.rg = rg;
-	}
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 
-	public String getCpf() {
-		return cpf;
-	}
+    public String getRg() {
+        return rg;
+    }
 
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
 
-	public Integer getMatricula() {
-		return matricula;
-	}
+    public String getCpf() {
+        return cpf;
+    }
 
-	public void setMatricula(Integer matricula) {
-		this.matricula = matricula;
-	}
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 
-	public Instituicao getInstituicao() {
-		return instituicao;
-	}
+    public Integer getMatricula() {
+        return matricula;
+    }
 
-	public void setInstituicao(Instituicao instituicao) {
-		this.instituicao = instituicao;
-	}
+    public void setMatricula(Integer matricula) {
+        this.matricula = matricula;
+    }
 
-	@Override
-	public String toString() {
-		return "Eleitor [cod=" + cod + ", nome=" + nome + ", dataNasc=" + dataNasc + ", telefone=" + telefone
-				+ ", endereco=" + endereco + ", rg=" + rg + ", cpf=" + cpf + ", matricula=" + matricula
-				+ ", instituicao=" + instituicao + "]";
-	}
-	
-	
+    public Instituicao getInstituicao() {
+        return instituicao;
+    }
+
+    public void setInstituicao(Instituicao instituicao) {
+        this.instituicao = instituicao;
+    }
+
+    @Override
+    public String toString() {
+        return "Eleitor [cod=" + cod + ", nome=" + nome + ", dataNasc=" + dataNasc + ", telefone=" + telefone
+                + ", endereco=" + endereco + ", rg=" + rg + ", cpf=" + cpf + ", matricula=" + matricula
+                + ", instituicao=" + instituicao + "]";
+    }
+
+    @Override
+    public int compareTo(Eleitor o) {
+        return rg.compareTo(o.getRg());
+    }
+
 }
